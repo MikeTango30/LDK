3f67956baa66eeeb142a817cb87db09ddf1876c0
Mantas Tarutis <mantas.tarutis@gmail.com>
2020-03-12 00:19:33 +0200

    layerslider resposive height, overlay height text not

---
+++ assets/js/layersliderHeight.js
@@ -0,0 +1,81 @@
+'use strict';
+
+(() => {
+    const xs = window.matchMedia('(min-width: 0px)');
+    const sm = window.matchMedia('(min-width: 576px)');
+    const md = window.matchMedia('(min-width: 768px)');
+    const lg = window.matchMedia('(min-width: 992px)');
+    const xl = window.matchMedia('(min-width: 1200px)');
+
+    const defaultLayerOneWidth = document.querySelector('img.ls-l:nth-child(2)').width;
+    const defaultLayerTwoWidth = document.querySelector('img.ls-l:nth-child(3)').width;
+    const slideText = document.querySelectorAll('.ls-wrapper');
+    console.log(slideText)
+
+    adjustLayerslider();
+    window.onresize = () => {
+        adjustLayerslider();
+    };
+
+    function adjustLayerslider () {
+        let layerslider = document.getElementById('layerslider');
+        let fullHeight = document.querySelector('.full-height').clientHeight;
+        let middleNavHeight = document.querySelector('.middle-nav').clientHeight;
+        let headerHeight = document.querySelector('header').clientHeight;
+
+        let layersliderHeight = fullHeight - middleNavHeight - headerHeight;
+        let layerSize = adjustLayerSizeForBreakpoints(layersliderHeight);
+
+        let firstLayers = document.querySelectorAll('img.ls-layer:nth-child(2)');
+        let secondLayers = document.querySelectorAll('img.ls-layer:nth-child(3)');
+
+        layerslider.style.height = `${layersliderHeight}px`;
+
+        firstLayers.forEach((layer) => {
+            layer.style.height = `${layerSize.layerHeight}px`;
+            layer.style.width = `${layerSize.layerOneWidth}px`;
+        });
+
+        secondLayers.forEach((layer) => {
+            layer.style.height = `${layerSize.layerHeight}px`;
+            layer.style.width = `${layerSize.layerTwoWidth}px`;
+        });
+    }
+
+    function adjustLayerSizeForBreakpoints(layersliderHeight) {
+        let layerHeight = layersliderHeight;
+        let layerOneWidth = defaultLayerOneWidth;
+        let layerTwoWidth = defaultLayerTwoWidth;
+
+        if (xs.matches) { // If media query matches
+            layerHeight = layersliderHeight + 1100;
+            layerOneWidth = defaultLayerOneWidth - 100;
+            layerTwoWidth = defaultLayerTwoWidth -100;
+        }
+        if (sm.matches) { // If media query matches
+            layerHeight = layersliderHeight + 600;
+            layerOneWidth = defaultLayerOneWidth - 200;
+            layerTwoWidth = defaultLayerTwoWidth -200;
+        }
+        if (md.matches) { // If media query matches
+            layerHeight = layersliderHeight + 400;
+            layerOneWidth = defaultLayerOneWidth - 250;
+            layerTwoWidth = defaultLayerTwoWidth -250;
+        }
+        if (lg.matches) { // If media query matches
+            layerHeight = layersliderHeight + 200;
+            layerOneWidth = defaultLayerOneWidth - 300;
+            layerTwoWidth = defaultLayerTwoWidth -300;
+        }
+        if (xl.matches) { // If media query matches
+            layerOneWidth = defaultLayerOneWidth;
+            layerTwoWidth = defaultLayerTwoWidth;
+        }
+
+        return {
+            layerHeight: layerHeight,
+            layerOneWidth: layerOneWidth,
+            layerTwoWidth: layerTwoWidth
+        };
+    }
+})();
