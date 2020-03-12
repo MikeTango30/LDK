66bed2ad64674466d811f033161aeb6d0d64454b
Mantas Tarutis <mantas.tarutis@gmail.com>
2020-03-12 08:54:58 +0200

    adjust header, promo, middle nav size for sticky header

--- assets/js/layersliderHeight.js
+++ assets/js/layersliderHeight.js
@@ -9,16 +9,37 @@
 
     const defaultLayerOneWidth = document.querySelector('img.ls-l:nth-child(2)').width;
     const defaultLayerTwoWidth = document.querySelector('img.ls-l:nth-child(3)').width;
-    const slideText = document.querySelectorAll('.ls-wrapper');
-    console.log(slideText)
+    // const slideText = document.querySelectorAll('.ls-wrapper');
+    // console.log(slideText)
 
-    adjustLayerslider();
+
+    //observe sticky header
+    const nav = document.querySelector('nav');
+    const config = {attributes: true};
+    const callback = function (mutationsList, observer) {
+        let fullHeight = document.querySelector('.full-height');
+        const layersliderHeight = document.getElementById('layerslider').clientHeight;
+        let middleNavHeight = document.querySelector('.middle-nav').clientHeight;
+        const headerHeight = document.querySelector('header').clientHeight;
+        let newFullHeight = layersliderHeight + middleNavHeight + headerHeight;
+
+
+        for (let mutation of mutationsList) {
+             mutation.target.classList.contains('sticky') ? fullHeight.style.height = `${newFullHeight}px`
+                 : fullHeight.style.height = '100vh';
+        }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(nav, config);
+
+
+    // adjustLayerslider();
     window.onresize = () => {
         adjustLayerslider();
     };
 
-    function adjustLayerslider () {
-        let layerslider = document.getElementById('layerslider');
+    function adjustLayerslider() {
+        const layerslider = document.getElementById('layerslider');
         let fullHeight = document.querySelector('.full-height').clientHeight;
         let middleNavHeight = document.querySelector('.middle-nav').clientHeight;
         let headerHeight = document.querySelector('header').clientHeight;
@@ -50,22 +71,22 @@
         if (xs.matches) { // If media query matches
             layerHeight = layersliderHeight + 1100;
             layerOneWidth = defaultLayerOneWidth - 100;
-            layerTwoWidth = defaultLayerTwoWidth -100;
+            layerTwoWidth = defaultLayerTwoWidth - 100;
         }
         if (sm.matches) { // If media query matches
             layerHeight = layersliderHeight + 600;
             layerOneWidth = defaultLayerOneWidth - 200;
-            layerTwoWidth = defaultLayerTwoWidth -200;
+            layerTwoWidth = defaultLayerTwoWidth - 200;
         }
         if (md.matches) { // If media query matches
             layerHeight = layersliderHeight + 400;
             layerOneWidth = defaultLayerOneWidth - 250;
-            layerTwoWidth = defaultLayerTwoWidth -250;
+            layerTwoWidth = defaultLayerTwoWidth - 250;
         }
         if (lg.matches) { // If media query matches
             layerHeight = layersliderHeight + 200;
             layerOneWidth = defaultLayerOneWidth - 300;
-            layerTwoWidth = defaultLayerTwoWidth -300;
+            layerTwoWidth = defaultLayerTwoWidth - 300;
         }
         if (xl.matches) { // If media query matches
             layerOneWidth = defaultLayerOneWidth;
